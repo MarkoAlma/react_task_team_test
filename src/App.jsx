32c57c0e15e41672de8bf2f8fbb794b4ed8d7c56
mkatay <kmagdi@kkando.hu>
2026-01-07 11:58:59 +0100

    DEV: AddTask.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AddTask } from './components/AddTask'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       ALMA
+      <AddTask/>
     </>
   )
 }
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddTask = () => {
+  return (
+    <div>
+      addTask....
+    </div>
+  )
+}
+
+
