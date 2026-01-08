1bc1f96a4e3b7b2abadaf22de1e15e43a927efdd
MarkoAlma <markogyapjas@gmail.com>
2026-01-08 12:34:22 +0100

    owner:final design

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,10 +3,13 @@ import TaskItem from './TaskItem'
 
 const TaskList = ({tasks, setTasks}) => {
   return (
-    <div>
+    <div style={{margin:'0 auto',}}>
+        
       {tasks.map((obj)=>
         <TaskItem key={obj.id} {...obj} setTasks={setTasks} tasks={tasks}/>
+        
     )}
+
     </div>
   )
 }
