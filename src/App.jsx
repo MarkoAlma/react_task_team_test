b6b9f21a9f12e228c45745f8bccd336fe52d6c0e
MarkoAlma <markogyapjas@gmail.com>
2026-01-08 11:03:06 +0100

    taskok

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { AddTask } from './components/AddTask'
+import TaskList from './components/TaskList'
+import { initialTasks } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState(initialTasks)
 
   return (
     <>
       ALMA
       <AddTask/>
+      <TaskList tasks={tasks} setTasks={setTasks}/>
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const initialTasks = [
+  { id: 1, text: "Első feladat", completed: false, assignedTo: "Anna" },
+  { id: 2, text: "Második feladat", completed: true, assignedTo: "Béla" },
+  { id: 3, text: "Harmadik feladat", completed: false, assignedTo: "Csaba" }
+];
